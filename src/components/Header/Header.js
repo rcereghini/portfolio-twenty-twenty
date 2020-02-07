37c92f7d8f4ab8836a29f3efb5ed20715eba1254
Robert Cereghini <rcereghini@gmail.com>
2020-02-07 01:25:13 -0700

    margin

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ContactInfo from "../ContactInfo/ContactInfo";
+import htmlLogo from "../../assets/logos/html.png";
+import sqlLogo from "../../assets/logos/mysql.svg";
+import cssLogo from "../../assets/logos/css.svg";
+import csharpLogo from "../../assets/logos/csharp.png";
+import firebaseLogo from "../../assets/logos/firebase.png";
+import jsLogo from "../../assets/logos/js.png";
+import vueLogo from "../../assets/logos/vue.png";
+import reactLogo from "../../assets/logos/react.png";
+import reduxLogo from "../../assets/logos/Redux.png";
 import "./header.css";
 
 import avatar from "../../assets/avatar.jpg";
@@ -9,20 +18,22 @@ const Header = () => {
     <div className="header-main">
       <img src={avatar} className="avatar-image"></img>
       <div className="logo-main">
-        <p>R</p>
-        <p>R</p>
-        <p>R</p>
-        <p>R</p>
-        <p>R</p>
-        <p>R</p>
-        <p>R</p>
-        <p>R</p>
-        <p>R</p>
-        <p>R</p>
-        <p>R</p>
-        <p>R</p>
-        <p>R</p>
-        <p>R</p>
+        <img className="logo-main" src={htmlLogo}></img>
+        <img className="logo-main" src={cssLogo}></img>
+        <img className="logo-main" src={jsLogo}></img>
+        <img className="logo-main" src={reactLogo}></img>
+        <img className="logo-main" src={reduxLogo}></img>
+        <img className="logo-main" src={vueLogo}></img>
+        <img className="logo-main" src={firebaseLogo}></img>
+        <img className="logo-main" src={csharpLogo}></img>
+        <img className="logo-main" src={sqlLogo}></img>
+        <img className="logo-main" src={vueLogo}></img>
+        <img className="logo-main" src={vueLogo}></img>
+        <img className="logo-main" src={vueLogo}></img>
+        <img className="logo-main" src={vueLogo}></img>
+        <img className="logo-main" src={vueLogo}></img>
+        <img className="logo-main" src={vueLogo}></img>
+        <img className="logo-main" src={vueLogo}></img>
       </div>
       <ContactInfo></ContactInfo>
     </div>
